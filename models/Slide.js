58167ced832269a9af15384535c5097f3f7b1cf8
Maxim <makskovalko@gmail.com>
2015-12-28 07:28:31 +0200

    full commit

--- models/Slide.js
+++ models/Slide.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var connection = mongoose.createConnection('mongodb://localhost/airslides');
+var connection = mongoose.createConnection(process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||'mongodb://localhost/airslides');
 var autoIncrement = require('mongoose-auto-increment');
 
 autoIncrement.initialize(connection);
