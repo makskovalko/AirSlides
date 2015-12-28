4473b941b640890a0071930e504c4ba40e62df89
Maxim <makskovalko@gmail.com>
2015-12-28 08:15:31 +0200

    full project

--- models/Vote.js
+++ models/Vote.js
@@ -1,5 +1,15 @@
 var mongoose = require('mongoose');
-var connection = mongoose.createConnection(process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||'mongodb://localhost/airslides');
+//var connection = mongoose.createConnection('mongodb://localhost/airslides');
+var options = {
+    db: { native_parser: true },
+    server: { poolSize: 5 },
+    replset: { rs_name: 'myReplicaSetName' },
+    user: 'admin',
+    pass: 'X6HbdLX6k75R'
+};
+
+var connection = mongoose.createConnection('mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/airslides', options);
+
 var autoIncrement = require('mongoose-auto-increment');
 
 autoIncrement.initialize(connection);
